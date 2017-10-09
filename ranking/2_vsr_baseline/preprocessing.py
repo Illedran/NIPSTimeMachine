6665835af36e0330c58d4ab31a0c695d186d0329
Informhunter <informhunter@gmail.com>
2017-10-09 14:40:36 +0200

    Tabs to spaces.

--- ranking/2_vsr_baseline/preprocessing.py
+++ ranking/2_vsr_baseline/preprocessing.py
@@ -3,44 +3,46 @@ import nltk
 #Uses NLTK word_toknizer, with conversion to lower case.
 class BasicTokenizer:
 
-	def tokenize(self, text):
-		return [t for t in nltk.word_tokenize(text.lower())]
+    def tokenize(self, text):
+        return [t for t in nltk.word_tokenize(text.lower())]
 
 #Filters out stopwords from NLTK english stopword list
 class BasicFilter:
 
-	def __init__(self):
-		self.stopwords = set(nltk.corpus.stopwords.words('english'))
+    def __init__(self):
+        self.stopwords = set(nltk.corpus.stopwords.words('english'))
 
-	def filt(self, tokens):
-		return [t for t in tokens if not t in self.stopwords and len(t) > 1 and t.isalnum()]
+    def filt(self, tokens):
+        def good(t):
+            return not t in self.stopwords and len(t) > 1 and t.isalnum() and not t.isdigit()
+        return [t for t in tokens if good(t)]
 
 #Stemmer that does nothing
 class NoStemmer:
 
-	def stem(self, tokens):
-		return tokens
+    def stem(self, tokens):
+        return tokens
 
 #Snowball stemmer from NLTK
 class SnowballStemmer:
 
-	def stem(self, tokens):
-		stemmer = nltk.stem.SnowballStemmer('english')
-		return [stemmer.stem(t) for t in tokens]
+    def stem(self, tokens):
+        stemmer = nltk.stem.SnowballStemmer('english')
+        return [stemmer.stem(t) for t in tokens]
 
 #Adjustible text preprocessing pipeline
 class Preprocessor:
 
-	def __init__(self):
-		self.tokenizer = BasicTokenizer()
-		self.filter = BasicFilter()
-		self.stemmer = SnowballStemmer()
+    def __init__(self):
+        self.tokenizer = BasicTokenizer()
+        self.filter = BasicFilter()
+        self.stemmer = SnowballStemmer()
 
-	def process(self, text):
-		tokens = self.tokenizer.tokenize(text)
-		tokens = self.filter.filt(tokens)
-		tokens = self.stemmer.stem(tokens)
-		return tokens
+    def process(self, text):
+        tokens = self.tokenizer.tokenize(text)
+        tokens = self.filter.filt(tokens)
+        tokens = self.stemmer.stem(tokens)
+        return tokens
 
-	def process_texts(self, texts=[]):
-		return [self.process(text) for text in texts]
+    def process_texts(self, texts=[]):
+        return [self.process(text) for text in texts]
--- ranking/2_vsr_baseline/ranking.py
+++ ranking/2_vsr_baseline/ranking.py
@@ -6,24 +6,24 @@ from preprocessing import Preprocessor
 
 class BasicVSRanker:
 
-	@staticmethod
-	def from_tokenized(texts, prepr=Preprocessor()):
-		vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=1000)
-		vectorized_texts = vectorizer.fit_transform([" ".join(text) for text in texts])
+    @staticmethod
+    def from_tokenized(texts, prepr=Preprocessor()):
+        vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=1000)
+        vectorized_texts = vectorizer.fit_transform([" ".join(text) for text in texts])
 
-		ranker = BasicVSRanker()
-		ranker.vectorizer = vectorizer
-		ranker.vectorized_texts = vectorized_texts
-		ranker.prepr = prepr
+        ranker = BasicVSRanker()
+        ranker.vectorizer = vectorizer
+        ranker.vectorized_texts = vectorized_texts
+        ranker.prepr = prepr
 
-		return ranker
+        return ranker
 
-	@staticmethod
-	def from_raw(texts, prepr=Preprocessor()):
-		tokenized = prepr.process_texts(texts)
-		return BasicVSRanker.from_tokenized(tokenized)
+    @staticmethod
+    def from_raw(texts, prepr=Preprocessor()):
+        tokenized = prepr.process_texts(texts)
+        return BasicVSRanker.from_tokenized(tokenized)
 
-	def best_n_matches(self, query, n=10):
-		query = self.prepr.process(query)
-		query = self.vectorizer.transform(query)
-		return np.argsort(-cosine_similarity(query, self.vectorized_texts)).flatten()[:n]
+    def best_n_matches(self, query, n=10):
+        query = self.prepr.process(query)
+        query = self.vectorizer.transform(query)
+        return np.argsort(-cosine_similarity(query, self.vectorized_texts)).flatten()[:n]
