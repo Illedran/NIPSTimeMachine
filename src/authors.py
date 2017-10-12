d9bd09b0b3ee9e6c6fd4d8c40d7586e8bfd1af7f
Guillermo Alonso <guialonun@gmail.com>
2017-10-12 17:38:09 +0200

    Finish extracting functionality into functions

--- src/authors.py
+++ src/authors.py
@@ -12,11 +12,6 @@ tokenizer = RegexpTokenizer(r'\w+')
 en_stop = get_stop_words('en')
 q = 'Michael I. Jordan'
 
-author_texts = []
-texts = []
-
-years = {}
-
 
 # Check for numbers in text
 def is_number(n):
@@ -86,26 +81,9 @@ def get_coauthors(relevant_papers, relevant_authors):
 
   return coauthors
 
-if __name__ == '__main__':
-
-  relevant_authors, author_variety = process_authors(q)
-
-  check_author_validity(author_variety, q)
-
-  relevant_papers = get_relevant_papers(relevant_authors)
-
-  coauthors = get_coauthors(relevant_papers, relevant_authors)
-
-  sorted_coauthors = sorted(coauthors, key=operator.itemgetter(1), reverse=True)
-
-  print('List of common co-authors:')
-
-  with open('../nips-data/authors.csv', 'r') as csv_file:
-    authors = csv.DictReader(csv_file)
-    for author in authors:
-      for coauthor in sorted_coauthors[:5]:
-        if author.get('id') == coauthor: print(author.get('name'))
-
+def get_author_texts_and_years(relevant_papers):
+  author_texts = []
+  years = defaultdict(lambda: 0)
 
   with open('../nips-data/papers.csv', 'r') as csv_file:
     papers = csv.DictReader(csv_file)
@@ -115,19 +93,12 @@ if __name__ == '__main__':
         if paper.get('id') == relevant_paper:
           author_texts.append(paper.get('abstract'))
           year = paper.get('year')
-          if year in years:
-            years[year] += 1
-          else:
-            years[year] = 1
+          years[year] += 1
 
-  sorted_years = sorted(years)
+  return (author_texts, years)
 
-  print('Count of papers by year:')
-
-  for year in range(int(sorted_years[0]), int(sorted_years[-1])):
-    year_str = str(year)
-    if year_str in years:
-      print(year_str + ': ' + str(years[year_str]))
+def get_text_keywords_sanitized(author_texts):
+  texts = []
 
   # LDA topic extraction
   # Remove stop words and stem
@@ -140,6 +111,10 @@ if __name__ == '__main__':
     terms = filter(lambda x: len(x) > 2 and not is_number(x), stopped_tokens)
     texts.append(list(terms))
 
+  return texts
+
+
+def build_models(texts):
   # Build models
   dictionary = gensim.corpora.Dictionary(texts)
   corpus = [dictionary.doc2bow(text) for text in texts]
@@ -151,3 +126,39 @@ if __name__ == '__main__':
   print(ldamodel.show_topics(10, 7))
   print('LSI:')
   print(lsimodel.print_topics(5))
+
+
+if __name__ == '__main__':
+
+  relevant_authors, author_variety = process_authors(q)
+
+  check_author_validity(author_variety, q)
+
+  relevant_papers = get_relevant_papers(relevant_authors)
+
+  coauthors = get_coauthors(relevant_papers, relevant_authors)
+
+  sorted_coauthors = sorted(coauthors, key=operator.itemgetter(1), reverse=True)
+
+  # Print some information
+  print('List of common co-authors:')
+  with open('../nips-data/authors.csv', 'r') as csv_file:
+    authors = csv.DictReader(csv_file)
+    for author in authors:
+      for coauthor in sorted_coauthors[:5]:
+        if author.get('id') == coauthor:
+          print(author.get('name'))
+
+  author_texts, years = get_author_texts_and_years(relevant_papers)
+
+  # Print some information
+  sorted_years = sorted(years)
+  print('Count of papers by year:')
+  for year in range(int(sorted_years[0]), int(sorted_years[-1])):
+    year_str = str(year)
+    if year_str in years:
+      print(year_str + ': ' + str(years[year_str]))
+
+  texts = get_text_keywords_sanitized(author_texts)
+
+  build_models(texts)
